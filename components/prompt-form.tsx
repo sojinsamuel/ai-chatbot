b2ebf69c35d8b862d2ddeea07e21acec64748a6f
sojinsamuel <sojinsamuel2001@gmail.com>
2024-06-18 06:09:17 +0530

    twilio commit 2

--- components/prompt-form.tsx
+++ components/prompt-form.tsx
@@ -81,7 +81,7 @@ export function PromptForm({
               <span className="sr-only">New Chat</span>
             </Button>
           </TooltipTrigger>
-          <TooltipContent>New Chat</TooltipContent>
+          <TooltipContent>New Chat interface</TooltipContent>
         </Tooltip>
         <Textarea
           ref={inputRef}
